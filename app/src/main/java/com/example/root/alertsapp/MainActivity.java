ab75efe69b84aad75d101fc3ceb77de04a3df914
anshcena <anshcena.1996@gmail.com>
2018-08-22 10:16:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/root/alertsapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.root.alertsapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText empid,pswd;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        empid=(EditText)findViewById(R.id.empid);
+        pswd=(EditText)findViewById(R.id.pswd);
+        login=(Button)findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Intent i=new Intent(getApplicationContext());
+            }
+        });
+
+
+
+
+    }
+}
