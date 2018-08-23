e21fdcd17faa0ce404a2c37cee617bf60b810e8b
anshcena <anshcena.1996@gmail.com>
2018-08-23 13:31:09 +0530

    screens

--- app/src/main/java/com/example/root/alertsapp/home.java
+++ app/src/main/java/com/example/root/alertsapp/home.java
@@ -1,14 +1,16 @@
 package com.example.root.alertsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class home extends AppCompatActivity {
 
 
-    Button createalert,myalerts,allalerts;
+    Button createalert1,myalert,allalerts;
     TextView logout;
 
     @Override
@@ -16,12 +18,29 @@ public class home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        createalert=(Button)findViewById(R.id.createalert);
-        myalerts=(Button)findViewById(R.id.myalert);
+        createalert1=(Button)findViewById(R.id.createalert);
+        myalert=(Button)findViewById(R.id.myalert);
         allalerts=(Button)findViewById(R.id.allalerts);
 
         logout=(TextView)findViewById(R.id.logout);
-        
+
+        createalert1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ii=new Intent(home.this,createalert.class);
+                startActivity(ii);
+            }
+        });
+
+        myalert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(home.this,myalerts.class);
+                startActivity(intent);
+
+            }
+        });
 
 
     }
